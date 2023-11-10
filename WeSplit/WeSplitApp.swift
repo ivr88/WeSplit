c4d727c003c269baf92f0cc29ee5e9e291aee697
ivr88 <114075652+ivr88@users.noreply.github.com>
2023-11-10 15:20:14 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Вадим Исламов on 10.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
